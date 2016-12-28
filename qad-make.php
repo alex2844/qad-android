5d07d6b2a8249cda4f8481c286323d2e6aebecfe
Alex Smith <alex.smith2844@gmail.com>
2016-12-28 11:18:31 +0500

    qad-make convert db && support cli

--- qad-make.php
+++ qad-make.php
@@ -10,6 +10,8 @@ Copyright (c) 2016 Alex Smith
 */
 header('Content-Type: text/html; charset=utf-8');
 session_start();
+if (!empty($argv[1]))
+	$_GET['qad-make'] = $argv[1];
 if (!empty($_GET['page'])) {
 	$method = (!empty($_GET['method']) ? $_GET['method'] : 'GET');
 	$parse = preg_replace([
@@ -18,14 +20,9 @@ if (!empty($_GET['page'])) {
 		'/&page=/',
 		'/page=/'
 	],'',$_SERVER['QUERY_STRING']);
-	if ($method == 'GET') {
-		$opts = ['http' => [
-			'method' => 'GET',
-			'header' => 'Cookie: PHPSESSID='.$_COOKIE['session']
-		]];
-		$context = stream_context_create($opts);
-		$res = file_get_contents($parse, 0, $context);
-	}else{
+	if ($method == 'GET')
+		$res = file_get_contents($parse);
+	else{
 		$parse = parse_url($parse);
 		$opts = ['http' => [
 			'method'  => $method,
@@ -48,71 +45,184 @@ if (!empty($_GET['page'])) {
 		}
 	echo $res;
 }else if (!empty($_GET['qad-make'])) {
-	$log = [];
-	$json = json_decode(file_get_contents('qad-make/'.$_GET['qad-make'].'.json'), true);
-	if (!empty($json['files'])) {
-		$log[] = implode(" ",$json['files']);
-		shell_exec('zip -r qad-make/'.$_GET['qad-make'].'.zip qad-make/'.$_GET['qad-make'].'.json '.implode(" ",$json['files']));
-	}
-	if (!empty($json['session']))
-		foreach ($json['session'] as $k=>$v)
-			$log['session'][$k] = $_SESSION[$k] = $v;
-	if (!empty($json['db'])) {
-		if (preg_match('/sqlite/', $json['db']['connect'])) {
-			$dbh = new PDO($json['db']['connect']);
-			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NAMED);
-			if (!empty($json['db']['create']))
-				foreach ($json['db']['create'] as $k=>$v) {
-					$sql = 'create table '.$k.' (';
-						$i = 1;
-						$count = count($v);
-						foreach ($v as $n=>$t) {
-							$sql .= '`'.$n.'` '.$t.($i < $count ? ', ' : '');
-							++$i;
-						}
-					$sql .= ')';
-					try {
-						$dbh->exec($sql);
-						$log['create'][] = $k;
-						if (!empty($json['db']['insert']) && !empty($json['db']['insert'][$k])) {
-							foreach ($json['db']['insert'][$k] as $exec) {
-								$sql = 'insert into '.$k.' (';
-								$count = count($exec);
-								for ($i = 1, $j = 0; $i <=2; ++$i)
-									foreach ($exec as $n=>$t) {
-										++$j;
-										if ($i == 1)
-											$sql .= $n;
-										else
-											$sql .= '"'.$t.'"';
-										if ($j < $count)
-											$sql .= ', ';
-										else{
-											if ($i == 1)
-												$sql .= ') values (';
-											$j = 0;
-										}
-									}
-								$sql .= ')';
-								$dbh->exec($sql);
-								$log['insert'][] = $k;
-							}
-						}
-					} catch (Exception $e) {}
+	if ($_GET['qad-make'] == 'convert') {
+		$vals = [];
+		$vars = ['MySQL database','Mysql User','Password','SQLite database file'];
+		if (gettype(STDIN) == 'resource')
+			foreach($vars as $var) {
+				echo 'Введите '.$var.':';
+				if ($var=='Password')
+					`stty -echo`;
+				$vals[] = trim(fgets(STDIN));
+				if ($var=='Password'){
+					`stty echo`;
+					echo "\n";
 				}
+			}
+		else if (isset($_GET['vals']))
+			$vals = $_GET['vals'];
+		else{
+			echo '<form><input type="hidden" name="qad-make" value="convert" />';
+			foreach($vars as $var) {
+				echo 'enter '.$var.':';
+				echo '<input name="vals[]" />';
+				echo '<br />';
+			}
+			echo '<input type="submit" /></form>';
+			exit;
 		}
-		echo '<pre>';
-		print_r($log);
-		echo '</pre>';
+		$pdoMy = new PDO('mysql:dbname='.$vals[0], $vals[1], $vals[2], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")) or die("can't connect to $myDb");
+		$pdoLi = new PDO('sqlite:'.$vals[3]) or die("can't connect to $liDb");
+		$creates = [];
+		$tbls = [];
+		$q = $pdoMy->query('SHOW TABLES');
+		while($d = $q->fetch())
+			$tbls[] = current($d);
+		foreach($tbls as $tbl) {
+			echo "creating table '$tbl'\n";
+			$q = $pdoMy->query("SELECT * FROM `$tbl` LIMIT 1");
+			$cols = [];
+			for ($i = 0; $i < $q->columnCount(); ++$i){
+				$m = (object) $q->getColumnMeta($i);
+				$type = (isset($m->native_type) ? $m->native_type : '');
+				$def = "\t".$m->name.' '.str_replace(['LONG'], ['integer'], $type);
+				if (in_array('unique_key',$m->flags))
+					$def.=" unique";
+				if (in_array('primary_key',$m->flags))
+					$def.=" primary key";
+				if ($pdoMy->query('SHOW COLUMNS FROM `'.$tbl.'` where `Field` = "'.$m->name.'" && `Extra` = "auto_increment"')->fetch()) {
+					$def .= ' autoincrement';
+				}else if (in_array('not_null',$m->flags))
+					$def.=" not null";
+				$cols[] = $def;
+			}
+			query("CREATE TABLE IF NOT EXISTS $tbl(\n".implode(",\n",$cols)."\n);\n",$pdoLi);
+		}
+		foreach($tbls as $tbl) {
+			echo "inserting data into '$tbl'\n";
+			$qs = [];
+			$i = 0;
+			$q = $pdoMy->query("SELECT * FROM `$tbl`");
+			while ($d = $q->fetch(5)) {
+				$vals = $keys = [];
+				foreach ($d as $k=>$v) {
+					if ($k=='queryString')
+						continue;
+					$keys[] = $k;
+					$vals[] = $pdoLi->quote($v);
+				}
+				$qs[]="INSERT OR IGNORE INTO $tbl (".implode(',',$keys).") VALUES  (".implode(',',$vals).");\n";
+				++$i;
+				if ($i>=5000) {
+					query($qs,$pdoLi);
+					$qs = [];
+					$i = 0;
+				}
+			}
+			query($qs,$pdoLi);
+		}
+	}else{
+		$log = [];
+		$json = json_decode(file_get_contents('qad-make/'.$_GET['qad-make'].'.json'), true);
+		if (!empty($json['files'])) {
+			$log[] = implode(" ",$json['files']);
+			shell_exec('zip -r qad-make/'.$_GET['qad-make'].'.zip qad-make/'.$_GET['qad-make'].'.json '.implode(" ",$json['files']));
+		}
+		if (!empty($json['session']))
+			foreach ($json['session'] as $k=>$v)
+				$log['session'][$k] = $_SESSION[$k] = $v;
+		if (!empty($json['db'])) {
+			if (preg_match('/sqlite/', $json['db']['connect'])) {
+				$dbh = new PDO($json['db']['connect']);
+				$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NAMED);
+				if (!empty($json['db']['create']))
+					foreach ($json['db']['create'] as $k=>$v) {
+						$sql = 'create table '.$k.' (';
+							$i = 1;
+							$count = count($v);
+							foreach ($v as $n=>$t) {
+								$sql .= '`'.$n.'` '.$t.($i < $count ? ', ' : '');
+								++$i;
+							}
+						$sql .= ')';
+						try {
+							$dbh->exec($sql);
+							$log['create'][] = $k;
+							if (!empty($json['db']['insert']) && !empty($json['db']['insert'][$k])) {
+								foreach ($json['db']['insert'][$k] as $exec) {
+									$sql = 'insert into '.$k.' (';
+									$count = count($exec);
+									for ($i = 1, $j = 0; $i <=2; ++$i)
+										foreach ($exec as $n=>$t) {
+											++$j;
+											if ($i == 1)
+												$sql .= $n;
+											else
+												$sql .= '"'.$t.'"';
+											if ($j < $count)
+												$sql .= ', ';
+											else{
+												if ($i == 1)
+													$sql .= ') values (';
+												$j = 0;
+											}
+										}
+									$sql .= ')';
+									$dbh->exec($sql);
+									$log['insert'][] = $k;
+								}
+							}
+						} catch (Exception $e) {}
+					}
+			}
+			echo '<pre>';
+			print_r($log);
+			echo '</pre>';
+		}
 	}
 }else{
+	if (gettype(STDIN) == 'resource')
+		echo 'php '.$argv[0].' convert //Конвертер Mysql -> Sqlite'."\n";
+	else
+		echo '<a href="?qad-make=convert"><button>Конвертер Mysql -> Sqlite</button></a><br /><br />';
 	if (!$dir = opendir('qad-make/'))
 		exit;
 	while ($file = readdir($dir)) {
 		if ($file != '.' && $file != '..' && pathinfo($file, PATHINFO_EXTENSION) == 'json') {
 			$json = json_decode(file_get_contents('qad-make/'.$file), true);
-			echo '<a href="?qad-make='.pathinfo($file, PATHINFO_FILENAME).'"><button>'.$json['title'].'</button></a>';
+			if (gettype(STDIN) == 'resource')
+				echo 'php '.$argv[0].' '.pathinfo($file, PATHINFO_FILENAME).' //'.$json['title']."\n";
+			else
+				echo '<a href="?qad-make='.pathinfo($file, PATHINFO_FILENAME).'"><button>'.$json['title'].'</button></a>';
+		}
+	}
+}
+function query($qs,$pdo){
+	if($pdo) {
+		if (is_array($qs))
+			$lock = 1;
+		else{
+			$lock = 0;
+			$qs = [$qs];
+		}
+		if ($lock) {
+			$pdo->exec('PRAGMA synchronous = 0;');
+			$pdo->exec('PRAGMA journal_mode = OFF;');
+			$pdo->exec('BEGIN;');
+		}
+		foreach ($qs as $q) {
+			$pdo->exec($q);
+		}
+		if ($lock) {
+			$pdo->exec('COMMIT;');
+			$pdo->exec('PRAGMA synchronous = FULL;');
+			$pdo->exec('PRAGMA journal_mode = DELETE;');
+		}
+		$err = $pdo->errorInfo();
+		if (intval($err[0])) {
+			echo "\nError: \n";
+			var_dump($q,$err);
 		}
 	}
 }
